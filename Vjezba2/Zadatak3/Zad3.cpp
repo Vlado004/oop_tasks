717b0a800b9326af5c937ce3d691be4f4d7327f0
Mateo Mamut <vladom156@gmail.com>
2019-03-17 19:32:49 +0100

    Vjezba2 update

--- Vjezba2/Zadatak3/Zad3.cpp
+++ Vjezba2/Zadatak3/Zad3.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
 
-typedef struct my_vec{
+typedef struct my_vect{
 	int *niz;
 	int fiz_vel, log_vel;
 
-	my_vec vector_new(int init) {
-
-		my_vec* vector = new my_vec;
+	my_vect *vector_new(int init) {
+		my_vect* vector = new my_vect;
 		vector->niz = new int[init];
+		vector->fiz_vel = init;
+		vector->log_vel = 0;
+		return vector;
 	}
 
-} my_vec;
+	void vector_delete() {
+		delete[] niz;
+	}
+
+} my_vect;
 
 int main() {
-
+	my_vect* vector = vector->vector_new(5);
+	vector->vector_delete();
 }
