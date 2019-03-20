782673e05b72c94947802bcc7d43edfbcd7303d6
Mateo Mamut <vladom156@gmail.com>
2019-03-20 14:58:18 +0100

    Zavrsena druga vjezba

--- Vjezba2/Zadatak2/Zad2.cpp
+++ Vjezba2/Zadatak2/Zad2.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int* poredaj(int* stari, int velicina) {
+	int* novi = new int[velicina];
+	int parni_index = 0, neparni_index = velicina - 1;
 
+	for (int i = 0; i < velicina; i++) {
+		if (stari[i] % 2) {
+			novi[neparni_index] = stari[i];
+			neparni_index--;
+		}
+		else {
+			novi[parni_index] = stari[i];
+			parni_index++;
+		}
+	}
+
+	return novi;
+}
+
+int main() {
+	int niz[] = { 15, 45, 98, 7, 146, 32, 159, 548, 18925, 21, 62 };
+	
+	int* novi = poredaj(niz, sizeof(niz) / sizeof(int));
+	for (int i = 0; i < sizeof(niz) / sizeof(int); i++) {
+		cout << novi[i] << endl;
+	}
+	system("pause");
 }
--- Vjezba2/Zadatak3/Zad3.cpp
+++ Vjezba2/Zadatak3/Zad3.cpp
@@ -1,25 +1,66 @@
 #include <iostream>
 using namespace std;
 
-typedef struct my_vect{
+typedef struct {
 	int *niz;
 	int fiz_vel, log_vel;
 
-	my_vect *vector_new(int init) {
-		my_vect* vector = new my_vect;
-		vector->niz = new int[init];
-		vector->fiz_vel = init;
-		vector->log_vel = 0;
-		return vector;
+	void vector_new() {
+		niz = new int[10];
+		fiz_vel = 10;
+		log_vel = 0;
 	}
 
 	void vector_delete() {
 		delete[] niz;
 	}
 
-} my_vect;
+	void vector_push_back(int broj) {
+		if (log_vel == fiz_vel) {
+			niz = (int*)realloc(niz, fiz_vel*2*sizeof(int));
+		}
+		niz[log_vel] = broj;
+		log_vel++;
+	}
+
+	int vector_pop_back() {
+		if (!log_vel)
+			return NULL;
+		log_vel = log_vel - 1;
+		return niz[log_vel];
+	}
+
+	int vector_size() {
+		return log_vel;
+	}
+
+	int& vector_front() {
+		return niz[0];
+	}
+
+	int& vector_back() {
+		return niz[log_vel - 1];
+	}
+
+} vector;
 
 int main() {
-	my_vect* vector = vector->vector_new(5);
-	vector->vector_delete();
+	vector* v = new vector;
+	v->vector_new();
+
+	for (int i = 0; i < 15; i++) {
+		v->vector_push_back(i);
+		cout << v->vector_size() << endl;
+	}
+
+	v->vector_front() = 21;
+	v->vector_back() = 56;
+
+	for (int i = 0; i < 15; i++) {
+		cout << v->vector_pop_back() << endl;
+	}
+
+	v->vector_delete();
+	delete v;
+	system("Pause");
 }
