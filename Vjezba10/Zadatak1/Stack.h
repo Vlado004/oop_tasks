4b9283018bc8ac0cceedc3fa18c7e940dbf1fef1
Mateo Mamut <vladom156@gmail.com>
2019-05-22 18:53:16 +0200

    Kraj 10 vjezbe

--- Vjezba10/Zadatak1/Stack.h
+++ Vjezba10/Zadatak1/Stack.h
@@ -1,5 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
+#include <iostream>
 
 template<typename T>
 class Stack {
@@ -7,7 +8,7 @@ private:
 	const int defaultSize = 10;
 	const int maxSize = 1000;
 	int size, top;
-	T arr;
+	T* arr;
 	T* stackPtr;
 public:
 	Stack() {
@@ -28,16 +29,47 @@ public:
 	~Stack() { delete[] arr; }
 
 	void push(T data) {
-		if (top >= size) //pomocna
+		if (this->full()) {
+			std::cout << "Nema dovoljno mjesta." << std::endl;
 			return;
-		stackPtr* = data;
+		}
+		*stackPtr = data;
 		top++;
 		stackPtr++;
 	}
-	T pop() {
-		if (top == 0) //pomocna i poruka
-			return NULL;
 
+	T pop() {
+		if (this->empty()) {
+			std::cout << "Stack je prazan." << std::endl;
+			return NULL;
+		}
+		top--;
+		stackPtr--;
+		return *stackPtr;
+	}
+
+	const bool full() {
+		if (top >= size)
+			return true;
+		return false;
+	}
+
+	const bool empty() {
+		if (top <= 0)
+			return true;
+		return false;
+	}
+
+	const T curr_top() {
+		if (this->empty()) {
+			std::cout << "Stack je prazan." << std::endl;
+			return NULL;
+		}
+		return arr[top - 1];
+	}
+
+	const int curr_size() {
+		return size;
 	}
 };
 
--- Vjezba10/Zadatak1/Zad1.cpp
+++ Vjezba10/Zadatak1/Zad1.cpp
@@ -1,7 +1,24 @@
 #include "Stack.h"
 
 int main() {
+	Stack<int> mem(3);
 
+	std::cout << "Stack je prazan -> " << mem.empty() << std::endl;
+	std::cout << "Stack je pun -> " << mem.full() << std::endl;
+	std::cout << "Velicina Stacka -> " << mem.curr_size() << std::endl;
+	std::cout << "Top element -> " << mem.curr_top() << std::endl << std::endl;
+	mem.push(3);
+	mem.push(4);
+	mem.push(5);
+	mem.push(6);
+	std::cout << "Stack je prazan -> " << mem.empty() << std::endl;
+	std::cout << "Stack je pun -> " << mem.full() << std::endl;
+	std::cout << "Velicina Stacka -> " << mem.curr_size() << std::endl;
+	std::cout << "Top element -> " << mem.curr_top() << std::endl << std::endl;
+	std::cout << mem.pop() << std::endl;
+	std::cout << mem.pop() << std::endl;
+	std::cout << mem.pop() << std::endl;
+	std::cout << mem.pop() << std::endl;
 
-	//system("pause");
+	system("pause");
 }
